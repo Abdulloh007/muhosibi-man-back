8d68c2bfd601a945bfdf855d0c1f03866643a09b
Abdulloh <sabirovaabdulloh2004@gmail.com>
2024-06-29 15:55:03 +0500

    v0.1.29

--- database/migrations/2024_01_21_103049_create_counterparties_table.php
+++ database/migrations/2024_01_21_103049_create_counterparties_table.php
@@ -14,19 +14,19 @@ return new class extends Migration
         Schema::create('counterparties', function (Blueprint $table) {
             $table->id();
             $table->text('full_name');
-            $table->string('short_name')->nullable();
-            $table->string('legal_address')->nullable();
-            $table->string('physic_address')->nullable();
+            $table->text('short_name')->nullable();
+            $table->text('legal_address')->nullable();
+            $table->text('physic_address')->nullable();
             $table->string('site')->nullable();
             $table->integer('category_id')->nullable();
             $table->integer('organization_id');
             $table->string('inn')->nullable();
             $table->string('kpp')->nullable();
-            $table->string('contacts')->nullable();
+            $table->text('contacts')->nullable();
             $table->string('for_sign_docs')->nullable();
             $table->string('by_person')->nullable();
-            $table->string('passport_details')->nullable();
-            $table->string('comment')->nullable();
+            $table->text('passport_details')->nullable();
+            $table->text('comment')->nullable();
             $table->enum('payment_method', ['Наличными','На карту зарплатного проекта',' На личную карту'])->nullable();
             $table->timestamps();
         });
